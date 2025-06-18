1aecf3088c3ca26b2af7580a29bd5ace60062271
SLM <slm@SLM-AIR.local>
2025-06-18 16:20:02 +0800

    only in chrome on web we'll open in iframe

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+import { useEffect } from "react";
+
 export default function AppPage() {
+  useEffect(() => {
+    const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+    // If not Chrome OR if mobile, redirect to the app directly
+    if (!isChrome || isMobile) {
+      window.location.href = "https://script.google.com/macros/s/AKfycbxzQyt4DRFwscUe5INomAYM2yDsYVbU1PuPkGJQyPSMb5pfPeL4XWxoDIBeOYmCoaax/exec";
+    }
+  }, []);
+  
+  // Only Chrome desktop users will see this
   return (
     <iframe
       src="https://script.google.com/macros/s/AKfycbxzQyt4DRFwscUe5INomAYM2yDsYVbU1PuPkGJQyPSMb5pfPeL4XWxoDIBeOYmCoaax/exec"
